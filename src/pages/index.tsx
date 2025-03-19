7c67b0a2fe618fae3092ab52f00b3e7a645126d7
Kraznik <103571066+Kraznik@users.noreply.github.com>
2025-03-19 17:40:42 +0530

    add wagmi and remove pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <ConnectButton />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
