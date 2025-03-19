a3382b8eba4c681c78c61cb0ddc71a5738865333
Kraznik <103571066+Kraznik@users.noreply.github.com>
2025-03-19 16:20:04 +0530

    Initial commit

---
+++ src/pages/earn-subpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EarnPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Earn Page</h1>
+      <p>This is the Earn page where users can earn rewards.</p>
+    </div>
+  );
+};
+
+export default EarnPage;
---
+++ src/pages/portfolio-subpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PortfolioPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+      <p>This is the Earn page where users can earn rewards.</p>
+    </div>
+  );
+};
+
+export default PortfolioPage;
