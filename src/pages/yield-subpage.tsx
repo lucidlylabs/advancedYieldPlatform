265d6bcefc65cd92ab8a15fafedc40beb0f36e93
Kraznik <103571066+Kraznik@users.noreply.github.com>
2025-03-29 14:21:19 +0530

    feat: earn page (yield selection page)
    1. Background state global on yield page diff states

--- src/pages/yield-subpage.tsx
+++ src/pages/yield-subpage.tsx
@@ -115,94 +115,9 @@ const MarketsSubpage = () => {
     setSelectedStrategy(null);
   };
 
-  if (selectedStrategy) {
-    return (
-      <DepositView
-        selectedAsset={selectedStrategy.asset}
-        duration={selectedStrategy.duration}
-        strategy={selectedStrategy.type}
-        apy={selectedStrategy.apy}
-        onBack={() => setSelectedStrategy(null)}
-      />
-    );
-  }
-
-  if (selectedAsset) {
-    const asset = selectedAsset.asset as AssetType;
-    return (
-      <div className="flex flex-col gap-8 justify-center items-center min-h-screen">
-        <h1>Select a asset you want yield on</h1>
-        <div className="flex gap-6 items-start">
-          <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-2">
-              <CustomCard
-                heading={asset}
-                imageSrc={`/images/icons/card-${asset.toLowerCase()}.svg`}
-                hoverColor={
-                  asset === "USD"
-                    ? "#B88AF8"
-                    : asset === "ETH"
-                    ? "#627EEA"
-                    : "#F7931A"
-                }
-                selectedDuration={selectedAsset.duration}
-              />
-              <button
-                onClick={resetSelection}
-                className="text-white opacity-60 hover:opacity-100 transition-all duration-200 text-sm text-center"
-              >
-                Change Asset
-              </button>
-            </div>
-          </div>
-          <div className="flex gap-6">
-            <div
-              onClick={() => handleStrategySelect("stable", asset)}
-              className="cursor-pointer"
-            >
-              <CustomCard
-                heading={`Stable ${asset}`}
-                imageSrc={`/images/icons/stable-${asset.toLowerCase()}.svg`}
-                hoverColor={
-                  asset === "USD"
-                    ? "#627EEA"
-                    : asset === "ETH"
-                    ? "#627EEA"
-                    : "#F7931A"
-                }
-                info={STRATEGY_INFO.stable[asset].description}
-                apy={STRATEGY_INFO.stable[asset].apy}
-                isStrategyCard={true}
-              />
-            </div>
-            <div
-              onClick={() => handleStrategySelect("incentive", asset)}
-              className="cursor-pointer"
-            >
-              <CustomCard
-                heading={`Incentives ${asset}`}
-                imageSrc={`/images/icons/incentives-${asset.toLowerCase()}.svg`}
-                hoverColor={
-                  asset === "USD"
-                    ? "#B88AF8"
-                    : asset === "ETH"
-                    ? "#627EEA"
-                    : "#F7931A"
-                }
-                info={STRATEGY_INFO.incentives[asset].description}
-                apy={STRATEGY_INFO.incentives[asset].apy}
-                isStrategyCard={true}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div 
-      className="h-[calc(100vh-128px)] flex flex-col gap-6 items-center pt-[8vh] relative overflow-hidden"
+      className="h-[calc(100vh-128px)] relative overflow-hidden"
       style={{
         backgroundImage: "url('/images/background/earn-page-bg.svg')",
         backgroundPosition: "bottom",
@@ -210,27 +125,107 @@ const MarketsSubpage = () => {
         backgroundSize: "100% auto"
       }}
     >
-      <h1 className="text-[32px] font-normal">Select a asset you want yield on</h1>
-      <div className="flex gap-6 justify-center items-center">
-        <CustomCard
-          heading="USD"
-          imageSrc="/images/icons/card-usd.svg"
-          hoverColor="#B88AF8"
-          onDurationSelect={(duration) => handleDurationSelect("USD", duration)}
+      {selectedStrategy ? (
+        <DepositView
+          selectedAsset={selectedStrategy.asset}
+          duration={selectedStrategy.duration}
+          strategy={selectedStrategy.type}
+          apy={selectedStrategy.apy}
+          onBack={() => setSelectedStrategy(null)}
         />
-        <CustomCard
-          heading="Ethereum"
-          imageSrc="/images/icons/card-eth.svg"
-          hoverColor="#627EEA"
-          onDurationSelect={(duration) => handleDurationSelect("ETH", duration)}
-        />
-        <CustomCard
-          heading="Bitcoin"
-          imageSrc="/images/icons/card-btc.svg"
-          hoverColor="#F7931A"
-          onDurationSelect={(duration) => handleDurationSelect("BTC", duration)}
-        />
-      </div>
+      ) : selectedAsset ? (
+        <div className="flex flex-col gap-8 justify-center items-center min-h-full">
+          <h1>Select a asset you want yield on</h1>
+          <div className="flex gap-6 items-start">
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <CustomCard
+                  heading={selectedAsset.asset as AssetType}
+                  imageSrc={`/images/icons/card-${(selectedAsset.asset as AssetType).toLowerCase()}.svg`}
+                  hoverColor={
+                    selectedAsset.asset === "USD"
+                      ? "#B88AF8"
+                      : selectedAsset.asset === "ETH"
+                      ? "#627EEA"
+                      : "#F7931A"
+                  }
+                  selectedDuration={selectedAsset.duration}
+                />
+                <button
+                  onClick={resetSelection}
+                  className="text-white opacity-60 hover:opacity-100 transition-all duration-200 text-sm text-center"
+                >
+                  Change Asset
+                </button>
+              </div>
+            </div>
+            <div className="flex gap-6">
+              <div
+                onClick={() => handleStrategySelect("stable", selectedAsset.asset as AssetType)}
+                className="cursor-pointer"
+              >
+                <CustomCard
+                  heading={`Stable ${selectedAsset.asset}`}
+                  imageSrc={`/images/icons/stable-${(selectedAsset.asset as AssetType).toLowerCase()}.svg`}
+                  hoverColor={
+                    selectedAsset.asset === "USD"
+                      ? "#627EEA"
+                      : selectedAsset.asset === "ETH"
+                      ? "#627EEA"
+                      : "#F7931A"
+                  }
+                  info={STRATEGY_INFO.stable[selectedAsset.asset as AssetType].description}
+                  apy={STRATEGY_INFO.stable[selectedAsset.asset as AssetType].apy}
+                  isStrategyCard={true}
+                />
+              </div>
+              <div
+                onClick={() => handleStrategySelect("incentive", selectedAsset.asset as AssetType)}
+                className="cursor-pointer"
+              >
+                <CustomCard
+                  heading={`Incentives ${selectedAsset.asset}`}
+                  imageSrc={`/images/icons/incentives-${(selectedAsset.asset as AssetType).toLowerCase()}.svg`}
+                  hoverColor={
+                    selectedAsset.asset === "USD"
+                      ? "#B88AF8"
+                      : selectedAsset.asset === "ETH"
+                      ? "#627EEA"
+                      : "#F7931A"
+                  }
+                  info={STRATEGY_INFO.incentives[selectedAsset.asset as AssetType].description}
+                  apy={STRATEGY_INFO.incentives[selectedAsset.asset as AssetType].apy}
+                  isStrategyCard={true}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-6 items-center pt-[8vh]">
+          <h1 className="text-[32px] font-normal">Select a asset you want yield on</h1>
+          <div className="flex gap-6 justify-center items-center">
+            <CustomCard
+              heading="USD"
+              imageSrc="/images/icons/card-usd.svg"
+              hoverColor="#B88AF8"
+              onDurationSelect={(duration) => handleDurationSelect("USD", duration)}
+            />
+            <CustomCard
+              heading="Ethereum"
+              imageSrc="/images/icons/card-eth.svg"
+              hoverColor="#627EEA"
+              onDurationSelect={(duration) => handleDurationSelect("ETH", duration)}
+            />
+            <CustomCard
+              heading="Bitcoin"
+              imageSrc="/images/icons/card-btc.svg"
+              hoverColor="#F7931A"
+              onDurationSelect={(duration) => handleDurationSelect("BTC", duration)}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
