60e0e46e4a49bd23231e414c74215505d689770c
Kraznik <103571066+Kraznik@users.noreply.github.com>
2025-03-29 17:31:12 +0530

    remove non needed rpc

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,13 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
-} from 'wagmi/chains';
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { base, mainnet, sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: "RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
   chains: [
     mainnet,
-    polygon,
-    optimism,
-    arbitrum,
     base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
   ],
   ssr: true,
 });
