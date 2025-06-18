5bc95543f11d0b94df628212a58c82805a471c35
Kraznik <103571066+Kraznik@users.noreply.github.com>
2025-06-18 20:46:25 +0100

    fix: build errors and add bitget wallet

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,9 +1,44 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import {
+  rainbowWallet,
+  bitgetWallet,
+  walletConnectWallet,
+  metaMaskWallet,
+  coinbaseWallet,
+  injectedWallet,
+} from '@rainbow-me/rainbowkit/wallets';
 import { base, mainnet, sepolia, Chain, arbitrum } from "wagmi/chains";
+import { createConfig, http } from 'wagmi';
 
-export const config = getDefaultConfig({
-  appName: "Advanced Yield Platform",
-  projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID || "YOUR_PROJECT_ID",
+const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID || 'YOUR_PROJECT_ID';
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [
+        rainbowWallet,
+        bitgetWallet,
+        metaMaskWallet,
+        coinbaseWallet,
+        walletConnectWallet,
+        injectedWallet,
+      ],
+    },
+  ],
+  {
+    appName: 'Advanced Yield Platform',
+    projectId,
+  }
+);
+
+export const config = createConfig({
+  connectors,
   chains: [mainnet, base, arbitrum],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+    [arbitrum.id]: http(),
+  },
   ssr: true,
 });
