5d43fe44f4d13b4d9eb3b2d4faf6828397315c84
Gyanshu Pathak <gyanshu_kp@mt.iitr.ac.in>
2025-06-22 20:26:04 +0530

    fixed: build error vannila-extract/next fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['@vanilla-extract', '@rainbow-me'],
   reactStrictMode: true,
+  experimental: {
+    esmExternals: 'loose', 
+  },
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
