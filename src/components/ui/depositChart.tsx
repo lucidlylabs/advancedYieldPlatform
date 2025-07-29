df6ec83e4432c479c87d522e25ce4d0155479ce1
Gyanshu Pathak <gyanshu_kp@mt.iitr.ac.in>
2025-07-29 16:04:57 +0530

    style:grid added to graph and tvl on right side of graph added

--- src/components/ui/depositChart.tsx
+++ src/components/ui/depositChart.tsx
@@ -5,6 +5,7 @@ import {
     YAxis,
     Tooltip,
     ResponsiveContainer,
+    CartesianGrid,
   } from "recharts";
   import { useEffect, useState } from "react";
   
@@ -57,50 +58,62 @@ import {
     return (
       <div className="p-6 rounded-xl text-white w-full">
         <div className="w-full h-[300px]">
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart data={data}>
-            <XAxis
-              dataKey="date"
-              tick={{ fill: "#A3A3A3", fontSize: 10 }}
-              axisLine={false}
-              tickLine={false}
-            />
-            <YAxis
-              tick={{ fill: "#A3A3A3", fontSize: 10 }}
-              axisLine={false}
-              tickLine={false}
-              tickFormatter={(val) => `$${val}`}
-            />
-            <Tooltip
-              contentStyle={{ backgroundColor: "#1C1D2A", border: "none" }}
-              labelStyle={{ color: "#A3A3A3" }}
-              formatter={(value) => [`$${value}`, ""]} 
-            />
-            {data.length > 0 && (() => {
-              const allKeys = Array.from(
-                new Set(
-                  data.flatMap((item) => Object.keys(item).filter((key) => key !== "date"))
-                )
-              );
-
-              const colors = {
-                [allKeys[0]]: "#7B5FFF", 
-                [allKeys[1]]: "#5CD6FF", 
-                [allKeys[2]]: "#C3F34A", 
-              };
-
-              return allKeys.map((key) => (
-                <Bar
-                  key={key}
-                  dataKey={key}
-                  stackId="a"
-                  fill={colors[key] || "#8884d8"} 
-                />
-              ));
-            })()}
-          </BarChart>
-        </ResponsiveContainer>
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              data={[...data].reverse()}
+              margin={{ top: 10, right: 40, left: 0, bottom: 0 }}
+            >
+              <CartesianGrid
+                strokeDasharray="3 3"
+                stroke="#2A2A3C"
+              />
+              <XAxis
+                dataKey="date"
+                tick={{ fill: "#A3A3A3", fontSize: 10 }}
+                axisLine={false}
+                tickLine={false}
+              />
+              <YAxis
+                orientation="right" 
+                tick={{ fill: "#A3A3A3", fontSize: 10 }}
+                axisLine={false}
+                tickLine={false}
+                tickFormatter={(val) => `$${val / 1_000_000}M`}
+              />
+              <Tooltip
+                contentStyle={{ backgroundColor: "#1C1D2A", border: "none" }}
+                labelStyle={{ color: "#A3A3A3" }}
+                formatter={(value) => [`$${value}`, ""]}
+              />
+              {data.length > 0 &&
+                (() => {
+                  const allKeys = Array.from(
+                    new Set(
+                      data.flatMap((item) =>
+                        Object.keys(item).filter((key) => key !== "date")
+                      )
+                    )
+                  );
+    
+                  const colors = {
+                    [allKeys[0]]: "#7B5FFF", // violet
+                    [allKeys[1]]: "#5CD6FF", // cyan
+                    [allKeys[2]]: "#C3F34A", // lime
+                  };
+    
+                  return allKeys.map((key) => (
+                    <Bar
+                      key={key}
+                      dataKey={key}
+                      stackId="a"
+                      fill={colors[key] || "#8884d8"}
+                      radius={[2, 2, 0, 0]} 
+                    />
+                  ));
+                })()}
+            </BarChart>
+          </ResponsiveContainer>
         </div>
-      </div>
+      </div>    
     );
   }
