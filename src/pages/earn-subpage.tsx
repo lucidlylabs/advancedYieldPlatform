d7ee892e91bdf81ad68e45d4bbd6e0a04eb89025
Gyanshu Pathak <gyanshu_kp@mt.iitr.ac.in>
2025-07-31 23:45:24 +0530

    style : earn page centered 2

--- src/pages/earn-subpage.tsx
+++ src/pages/earn-subpage.tsx
@@ -386,7 +386,7 @@ const YieldSubpage: React.FC<YieldSubpageProps> = ({ depositParams }) => {
           </div>
         </div>
       ) : (
-        <div className="flex flex-col min-h-screen items-center justify-center gap-6 pb-[12vh]">
+        <div className="flex flex-col min-h-screen items-center justify-center gap-6 pb-[14vh]">
           <h1 className="text-[20px] font-[619px] sm:text-[40px]">
             Select an asset you want yield on
           </h1>
